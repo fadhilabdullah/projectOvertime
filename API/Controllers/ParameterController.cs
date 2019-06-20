8129c61bc27c2c0cdd954f709daf2358261c88b2
Farin <mfarinakhsanta@gmail.com>
2019-06-20 09:28:06 +0700

    New Controller

---
+++ API/Controllers/ParameterController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLogic.Service;
+using DataAccess.Context;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class ParameterController : ApiController
+    {
+        private MyContext db = new MyContext();
+        public ParameterController() { }
+        //interface
+        private readonly IParameterService iParameterService;
+        public ParameterController(IParameterService _iParameterService)
+        {
+            iParameterService = _iParameterService;
+        }
+
+        public List<Parameter> GetParameter()
+        {
+            return iParameterService.Get();
+        }
+    }
+}
